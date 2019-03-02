613e58758de5138a181bc7edef044f8810ee7536
JasonJson <daniil.sorokin.228@gmail.com>
2019-03-02 19:00:30 +0300

    Add five lab

---
+++ WindowsForms/WindowsForms/Form5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void справкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\Users\niga_\Documents\HelpAndManual Projects\NewProject.chm");
+        }
+    }
+}
