b19ff21a494c3ac208301be5e788156a314ceef3
JasonJson <daniil.sorokin.228@gmail.com>
2019-03-05 22:05:09 +0300

    Add 4-6 labs of two part

--- WindowsForms/WindowsForms/Form7.Designer.cs
+++ WindowsForms/WindowsForms/Form7.Designer.cs
@@ -31,12 +31,41 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(203, 104);
+            this.label1.Location = new System.Drawing.Point(88, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(16, 17);
             this.label1.TabIndex = 0;
@@ -44,7 +73,8 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(420, 135);
+            this.textBox1.Location = new System.Drawing.Point(92, 43);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 1;
@@ -52,20 +82,311 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(527, 135);
+            this.label2.Location = new System.Drawing.Point(199, 47);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(46, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 16);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Laba 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 47);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Laba 2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 96);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Laba 3";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 252);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 17);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Laba 4";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 344);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 17);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Laba 5";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(400, 9);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 17);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Laba 6";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(336, 96);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(336, 132);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(336, 164);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(132, 22);
+            this.textBox4.TabIndex = 4;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(88, 100);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(233, 17);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "День рождения первого человека";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(89, 135);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(232, 17);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "День рождения второго человека";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(88, 167);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(239, 17);
+            this.label11.TabIndex = 5;
+            this.label11.Text = "День рождения третьего человека";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 194);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Результат";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(490, 135);
+            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 17);
+            this.label12.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(85, 253);
+            this.textBox5.MaxLength = 4;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(191, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(403, 258);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(158, 22);
+            this.textBox6.TabIndex = 11;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(403, 300);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(158, 22);
+            this.textBox7.TabIndex = 11;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(237, 300);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(160, 17);
+            this.label14.TabIndex = 12;
+            this.label14.Text = "Минимальный элемент";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(307, 258);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(90, 17);
+            this.label15.TabIndex = 12;
+            this.label15.Text = "Содержание";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(91, 353);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Сгенерировать";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(237, 356);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 17);
+            this.label13.TabIndex = 14;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(237, 389);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(54, 17);
+            this.label16.TabIndex = 15;
+            this.label16.Text = "label16";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(103, 389);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(128, 17);
+            this.label17.TabIndex = 16;
+            this.label17.Text = "Сумма элементов ";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(435, 40);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 28);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Сгенерировать";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(716, 194);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(0, 17);
+            this.label19.TabIndex = 19;
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(559, 194);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(151, 17);
+            this.label20.TabIndex = 19;
+            this.label20.Text = "Минимальная сумма: ";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(641, 46);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(0, 17);
+            this.label18.TabIndex = 20;
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(559, 43);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(0, 17);
+            this.label21.TabIndex = 21;
             // 
             // Form7
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form7";
             this.Text = "Form7";
             this.ResumeLayout(false);
@@ -78,5 +399,34 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label21;
     }
 }
--- WindowsForms/WindowsForms/Form7.cs
+++ WindowsForms/WindowsForms/Form7.cs
@@ -12,25 +12,45 @@ namespace WindowsForms
 {
     public partial class Form7 : Form
     {
-        double x = 3.981 * Math.Pow(10, -2);
-        double y = -1.625 * Math.Pow(10, 3);
-        double z = 0.512;
-        //double a = 1.26185;
+        List<int> list;
         public Form7()
         {
             InitializeComponent();
             textBox1.KeyPress += TextBox1_KeyPress;
             textBox1.TextChanged += TextBox1_TextChanged;
+            textBox2.KeyPress += TextBox2_KeyPress;
+            textBox3.KeyPress += TextBox2_KeyPress;
+            textBox4.KeyPress += TextBox2_KeyPress;
+            button4.Click += Button4_Click;
+            button1.Click += Button1_Click;
+            button3.Click += Button3_Click;
+            //Laba 4
+            list = new List<int>();
+            textBox5.KeyPress += TextBox1_KeyPress;
+            button2.Click += Button2_Click;
+            //Laba 1
+            double x = 3.981 * Math.Pow(10, -2);
+            double y = -1.625 * Math.Pow(10, 3);
+            double z = 0.512;
             double resault = Math.Pow(2, -x) * (Math.Sqrt(x + (Math.Pow(Math.Abs(y), 1 / 4)))) * (Math.Pow(Math.Pow(Math.E, x - 1 / Math.Sin(z)), 1 / 3));
             label1.Text = resault.ToString();
         }
 
+      
+
+
+
+
+
+
+
+        //Laba 2
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
             int k;
             if (textBox1.TextLength >= 3)
             {
-                k = textBox1.Text[textBox1.TextLength - 3];  label2.Text = k.ToString(); 
+                k = int.Parse(textBox1.Text[textBox1.TextLength - 3].ToString());  label2.Text = k.ToString(); 
             }
         }
 
@@ -40,5 +60,115 @@ namespace WindowsForms
             if (!Char.IsDigit(number) && number != 8)
                 e.Handled = true;
         }
+
+    
+
+        //Laba 3
+        private void Button1_Click(object sender, EventArgs e)
+        {
+          if (int.Parse(textBox2.Text) > 31  || int.Parse(textBox3.Text) > 31 ||  int.Parse(textBox4.Text) > 31  || (int.Parse(textBox2.Text) == int.Parse(textBox3.Text) && int.Parse(textBox3.Text) == int.Parse(textBox4.Text)))
+            {
+                label12.Text = "Некорректное значение";
+            } 
+            else
+                label12.Text = Math.Min(int.Parse(textBox2.Text), Math.Min(int.Parse(textBox3.Text), int.Parse(textBox4.Text))).ToString();
+        }
+
+        private void TextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (number == 48 || !Char.IsDigit(number) && number != 8)
+                e.Handled = true;
+        }
+        
+        //Laba 4
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            list.Add(int.Parse(textBox5.Text));
+            if (list.First() != 0)
+                textBox6.Text += list.Last() + ",";
+            else
+            {
+                DialogResult result = MessageBox.Show("Первый элемент равен 0! Попробуйте снова.", "Ошибка");
+                if (result == DialogResult.OK) list.Remove(0);
+            }
+
+            if (list.Contains(0))
+            {
+            int min = list.Max();
+            for (int i =0; i < list.IndexOf(0);i++)
+            {
+                if (list[i] < min)
+                {
+                    min = list[i];
+                }
+            }
+            textBox7.Text = min.ToString();
+            }
+        }
+
+        //Laba 5
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            int resault = 0;
+            Random random = new Random();
+            int[] mas = new int[10];
+            label13.Text = "";
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = random.Next(-10, 20);
+                label13.Text += mas[i].ToString() + ",";
+            }
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] < 0)
+                {
+                    for (int j = i + 1; j < mas.Length; j++)
+                    {
+                        resault +=Math.Abs(mas[j]);
+                    }
+                    break;
+                }
+            }
+            label16.Text = resault.ToString();
+        }
+        //Laba 6
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            int resault = 0;
+            Random random = new Random();
+            int[] sum = new int[5];
+            int[,] mas = new int[5,5];
+            label18.Text = "";
+            label21.Text = "";
+            label19.Text = "";
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    mas[i,j] = random.Next(-10, 20);
+                    sum[i] += mas[i, j];
+                    label18.Text += mas[i,j].ToString() + "  ";
+                }
+                label21.Text += sum[i].ToString() + "\n";
+                label18.Text += "\n";
+            }
+            label19.Text = sum.Min().ToString();
+            //for (int i = 0; i < mas.Length; i++)
+            //{
+            //    if (mas[i] < 0)
+            //    {
+            //        for (int j = i; j < mas.Length; j++)
+            //        {
+            //            resault += mas[j];
+            //        }
+            //        break;
+            //    }
+            //}
+            //label16.Text = resault.ToString();
+        }
+
+     
     }
 }
